fa96ab367893e650849e33074afa530bed957bb6
Vitaly Yermakov <vit_ermakov@mail.ru>
2022-10-25 16:21:33 +0300

    func ExecutePipeline

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -9,6 +9,40 @@ type (
 type Stage func(in In) (out Out)
 
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	// Place your code here.
-	return nil
+	// Вспомогательная функция для внедрения done-канала
+	executeStage := func(in In, done In, stage Stage) Out {
+		// промежуточный канал для переброски потока в stage
+		middleCh := make(Bi)
+		outCh := stage(middleCh)
+
+		go func() {
+			defer close(middleCh)
+			// цикл переброски из in в middleCh либо до done, либо до закрытия in
+			for {
+				select {
+				case <-done:
+					return
+				case ifValue, ok := <-in:
+					if !ok {
+						return
+					}
+					// на семинаре рекомендовали делать доп. select
+					select {
+					case <-done:
+						return
+					case middleCh <- ifValue:
+					}
+				}
+			}
+		}()
+
+		return outCh
+	}
+
+	// строим пайплайн
+	stream := in
+	for _, stage := range stages {
+		stream = executeStage(stream, done, stage)
+	}
+	return stream
 }
