d201c8f236c4763499b949922a95259d88925433
Vitaly Yermakov <vit_ermakov@mail.ru>
2022-12-08 02:12:23 +0300

    main Validate func

--- hw09_struct_validator/validator.go
+++ hw09_struct_validator/validator.go
@@ -4,27 +4,6 @@ import (
 	"github.com/vitermakov/otusgo-hw/hw09_struct_validator/validator"
 )
 
-/*
-	func main() {
-		var u = User1{
-			Role: "admin",
-			Response: struct {
-				Code int `validate:"in:200,400,404"`
-				Body string
-			}{
-				Code: 200,
-				Body: "",
-			},
-			Address: Address{
-				Zipcode: "555934",
-			},
-			Email: "dd@yandex.ru",
-			Age:   22,
-		}
-		err := Validate(u)
-		fmt.Println(err)
-	}
-*/
 func Validate(v interface{}) error {
 	return validator.ValidateStruct(v)
 }
