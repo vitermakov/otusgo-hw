e2699eb41ca02c866a7e2101de13135d2dabd1a6
Vitaly Yermakov <vit_ermakov@mail.ru>
2022-12-10 23:04:47 +0300

    making env_reader, executor

--- hw08_envdir_tool/env_reader.go
+++ hw08_envdir_tool/env_reader.go
@@ -1,5 +1,13 @@
 package main
 
+import (
+	"bufio"
+	"io"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
 type Environment map[string]EnvValue
 
 // EnvValue helps to distinguish between empty files and files with the first empty line.
@@ -11,6 +19,61 @@ type EnvValue struct {
 // ReadDir reads a specified directory and returns map of env variables.
 // Variables represented as files where filename is name of variable, file first line is a value.
 func ReadDir(dir string) (Environment, error) {
-	// Place your code here
-	return nil, nil
+	env := make(Environment)
+	err := filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
+		var value EnvValue
+		if err != nil {
+			return err
+		}
+
+		name := info.Name()
+		if !info.Mode().IsRegular() ||
+			strings.ContainsAny(name, ";=") {
+			return nil
+		}
+		// по заданию имеется в виду, что если файл полностью пустой, переменная помечается
+		// на удаление, т.е. мы не учитываем, что значение может стать пустым после TrimRight
+		if info.Size() == 0 {
+			value.NeedRemove = true
+		} else {
+			v, err := readFirstLine(path)
+			if err != nil {
+				return err
+			}
+			value.Value = clearValue(v)
+		}
+
+		env[name] = value
+
+		return nil
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	return env, nil
+}
+
+func clearValue(value string) string {
+	value = strings.TrimRight(value, " \t\r")
+	return strings.Replace(value, "\x00", "\n", -1)
+}
+
+func readFirstLine(filePath string) (string, error) {
+	handle, err := os.Open(filePath)
+	if err != nil {
+		return "", err
+	}
+	defer func() {
+		_ = handle.Close()
+	}()
+
+	reader := bufio.NewReader(handle)
+	bs, _, err := reader.ReadLine()
+	if err != nil && err != io.EOF {
+		return "", err
+	}
+
+	return string(bs), nil
 }
--- hw08_envdir_tool/executor.go
+++ hw08_envdir_tool/executor.go
@@ -1,7 +1,49 @@
 package main
 
+import (
+	"log"
+	"os"
+	"os/exec"
+)
+
 // RunCmd runs a command + arguments (cmd) with environment variables from env.
 func RunCmd(cmd []string, env Environment) (returnCode int) {
-	// Place your code here.
+	// при ошибках, возникающих до запуска подпроцесса вернем код = 1.
+	returnCode = 1
+
+	if len(cmd) == 0 {
+		log.Println("cmd not set")
+		return
+	}
+
+	for key, value := range env {
+		// если переменная уже существует сбросим ее.
+		_, exists := os.LookupEnv(key)
+		if exists {
+			if err := os.Unsetenv(key); err != nil {
+				log.Printf("error unset env var %s\n", key)
+				return
+			}
+		}
+		if value.NeedRemove {
+			continue
+		}
+		if err := os.Setenv(key, value.Value); err != nil {
+			log.Printf("error set env var %s\n", key)
+			return
+		}
+	}
+
+	execCmd := exec.Command(cmd[0], cmd[1:]...)
+	execCmd.Stdout = os.Stdout
+	execCmd.Stderr = os.Stderr
+	execCmd.Stderr = os.Stderr
+
+	if err := execCmd.Run(); err != nil {
+		if exitErr, ok := err.(*exec.ExitError); ok {
+			returnCode = exitErr.ExitCode()
+		}
+	}
+
 	return
 }
