6d8e38a108d51990b56f0b0b219205b41a10f94b
Vitaly Yermakov <vit_ermakov@mail.ru>
2023-01-20 22:14:38 +0300

    TestNotify

--- hw12_13_14_15_calendar/tests/main_test.go
+++ hw12_13_14_15_calendar/tests/main_test.go
@@ -43,20 +43,20 @@ func (ms *MainSuiteTest) TestCreate() {
 	nfTermOk := "3h"
 	nfTermErr := "6q"
 	testCases := []struct {
-		name              string
-		inputCreate       dto.EventCreate
-		checkResponseBody func(*dto.Event, error)
+		name          string
+		inputCreate   dto.EventCreate
+		checkResponse func(*dto.Event, error)
 	}{
 		{
 			name: "event add ok",
 			inputCreate: dto.EventCreate{
 				Title:       "Встреча в Zoom",
-				Date:        dateInFuture.String(),
+				Date:        dateInFuture.Format(time.RFC3339),
 				Duration:    "45m",
 				Description: &desc,
 				NotifyTerm:  &nfTermOk,
 			},
-			checkResponseBody: func(event *dto.Event, rErr error) {
+			checkResponse: func(event *dto.Event, rErr error) {
 				ms.Suite.Require().NoError(rErr)
 				_, err := uuid.Parse(event.ID)
 				ms.Suite.Require().NoError(err)
@@ -68,11 +68,11 @@ func (ms *MainSuiteTest) TestCreate() {
 		}, {
 			name:        "event empty",
 			inputCreate: dto.EventCreate{},
-			checkResponseBody: func(event *dto.Event, rErr error) {
+			checkResponse: func(event *dto.Event, rErr error) {
 				ms.Suite.Require().Nil(event)
 				invErr := errx.Invalid{}
 				ms.Suite.Require().ErrorAs(rErr, &invErr)
-				ms.Suite.Require().Len(invErr.Errors, 2)
+				ms.Suite.Require().Len(invErr.Errors(), 2)
 			},
 		}, {
 			name: "event wrong data",
@@ -82,24 +82,24 @@ func (ms *MainSuiteTest) TestCreate() {
 				Duration:   "45q",
 				NotifyTerm: &nfTermErr,
 			},
-			checkResponseBody: func(event *dto.Event, rErr error) {
+			checkResponse: func(event *dto.Event, rErr error) {
 				ms.Suite.Require().Nil(event)
 				invErr := errx.Invalid{}
 				ms.Suite.Require().ErrorAs(rErr, &invErr)
-				ms.Suite.Require().Len(invErr.Errors, 3)
+				ms.Suite.Require().Len(invErr.Errors(), 3)
 			},
 		}, {
 			name: "duplicate entry",
 			inputCreate: dto.EventCreate{
 				Title:    "Встреча в Zoom",
-				Date:     dateInFuture.Add(time.Minute * 10).String(),
+				Date:     dateInFuture.Add(time.Minute * 10).Format(time.RFC3339),
 				Duration: "50m",
 			},
-			checkResponseBody: func(event *dto.Event, rErr error) {
+			checkResponse: func(event *dto.Event, rErr error) {
 				ms.Suite.Require().Nil(event)
 				logErr := errx.Logic{}
 				ms.Suite.Require().ErrorAs(rErr, &logErr)
-				ms.Suite.Require().Len(logErr.Code(), model.ErrEventDateBusyCode)
+				ms.Suite.Require().Equal(model.ErrEventDateBusyCode, logErr.Code())
 			},
 		},
 	}
@@ -111,7 +111,7 @@ func (ms *MainSuiteTest) TestCreate() {
 			if event != nil {
 				ms.eventIds = append(ms.eventIds, event.ID)
 			}
-			tc.checkResponseBody(event, err)
+			tc.checkResponse(event, err)
 		})
 	}
 }
@@ -125,7 +125,7 @@ func (ms *MainSuiteTest) TestGetByID() {
 	nfTermOk := "3h"
 	newEvent, err := ms.client.Create(ctx, dto.EventCreate{
 		Title:       "Test event",
-		Date:        dateInFuture.String(),
+		Date:        dateInFuture.Format(time.RFC3339),
 		Duration:    "45m",
 		Description: &desc,
 		NotifyTerm:  &nfTermOk,
@@ -135,14 +135,14 @@ func (ms *MainSuiteTest) TestGetByID() {
 	ms.Suite.Require().NoError(err)
 
 	testCases := []struct {
-		name              string
-		ID                string
-		checkResponseBody func(event *dto.Event, err error)
+		name          string
+		ID            string
+		checkResponse func(event *dto.Event, err error)
 	}{
 		{
 			name: "exists event",
 			ID:   newEvent.ID,
-			checkResponseBody: func(event *dto.Event, rErr error) {
+			checkResponse: func(event *dto.Event, rErr error) {
 				ms.Suite.Require().NotNil(event)
 				ms.Suite.Require().NoError(rErr)
 				ms.Suite.Require().Equal(newEvent.ID, event.ID)
@@ -150,16 +150,16 @@ func (ms *MainSuiteTest) TestGetByID() {
 		}, {
 			name: "wrong event ID",
 			ID:   "ascascas",
-			checkResponseBody: func(event *dto.Event, rErr error) {
-				ms.Suite.Require().NotNil(event)
+			checkResponse: func(event *dto.Event, rErr error) {
+				ms.Suite.Require().Nil(event)
 				logErr := errx.Logic{}
 				ms.Suite.Require().ErrorAs(rErr, &logErr)
 			},
 		}, {
 			name: "not exists event",
 			ID:   uuid.New().String(),
-			checkResponseBody: func(event *dto.Event, rErr error) {
-				ms.Suite.Require().NotNil(event)
+			checkResponse: func(event *dto.Event, rErr error) {
+				ms.Suite.Require().Nil(event)
 				invErr := errx.NotFound{}
 				ms.Suite.Require().ErrorAs(rErr, &invErr)
 			},
@@ -167,8 +167,9 @@ func (ms *MainSuiteTest) TestGetByID() {
 	}
 
 	for _, tc := range testCases {
+		tc := tc
 		ms.Suite.Run(tc.name, func() {
-			tc.checkResponseBody(ms.client.GetByID(ctx, tc.ID))
+			tc.checkResponse(ms.client.GetByID(ctx, tc.ID))
 		})
 	}
 }
@@ -181,11 +182,11 @@ func (ms *MainSuiteTest) TestUpdate() {
 	insertInput := []dto.EventCreate{
 		{
 			Title:    "Test event 1",
-			Date:     dateInFuture.String(),
+			Date:     dateInFuture.Format(time.RFC3339),
 			Duration: "60m",
 		}, {
 			Title:    "Test event 2",
-			Date:     dateInFuture.Add(time.Hour * 2).String(),
+			Date:     dateInFuture.Add(time.Hour * 2).Format(time.RFC3339),
 			Duration: "60m",
 		},
 	}
@@ -198,12 +199,12 @@ func (ms *MainSuiteTest) TestUpdate() {
 		events = append(events, *newEvent)
 	}
 	titleOk := "Test event 2 updated"
-	dateOk := dateInFuture.Add(time.Hour * 4).String()
+	dateOk := dateInFuture.Add(time.Hour * 4).Format(time.RFC3339)
 	durOk := "90m"
 	desc := "new description"
 	nfTermOk := "3h"
 
-	dateDup := dateInFuture.Add(time.Minute * 30 * -1).String()
+	dateDup := dateInFuture.Add(time.Minute * 30 * -1).Format(time.RFC3339)
 	durDup := "60m"
 
 	titleErr := ""
@@ -212,10 +213,10 @@ func (ms *MainSuiteTest) TestUpdate() {
 	nfTermErr := "6o"
 
 	testCases := []struct {
-		name              string
-		inputUpdate       dto.EventUpdate
-		ID                string
-		checkResponseBody func(error)
+		name          string
+		inputUpdate   dto.EventUpdate
+		ID            string
+		checkResponse func(error)
 	}{
 		{
 			name: "event update ok",
@@ -227,7 +228,7 @@ func (ms *MainSuiteTest) TestUpdate() {
 				NotifyTerm:  &nfTermOk,
 			},
 			ID: events[1].ID,
-			checkResponseBody: func(rErr error) {
+			checkResponse: func(rErr error) {
 				ms.Suite.Require().NoError(rErr)
 				event, err := ms.client.GetByID(ctx, events[1].ID)
 				ms.Suite.Require().NotNil(event)
@@ -236,10 +237,12 @@ func (ms *MainSuiteTest) TestUpdate() {
 				// убедиться что событие обновилось
 				ms.Suite.Require().Equal(events[1].ID, event.ID)
 				ms.Suite.Require().Equal(titleOk, event.Title)
-				ms.Suite.Require().Equal(dateOk, event.Date.String())
-				ms.Suite.Require().Equal(durOk, event.Duration)
+				ms.Suite.Require().Equal(dateOk, event.Date.Format(time.RFC3339))
+				dv, _ := time.ParseDuration(durOk)
+				ms.Suite.Require().Equal(dv.String(), event.Duration)
 				ms.Suite.Require().Equal(desc, event.Description)
-				ms.Suite.Require().Equal(nfTermOk, event.NotifyTerm)
+				dv, _ = time.ParseDuration(nfTermOk)
+				ms.Suite.Require().Equal(dv.String(), event.NotifyTerm)
 			},
 		}, {
 			name: "event move to occupied date",
@@ -248,10 +251,10 @@ func (ms *MainSuiteTest) TestUpdate() {
 				Duration: &durDup,
 			},
 			ID: events[1].ID,
-			checkResponseBody: func(rErr error) {
+			checkResponse: func(rErr error) {
 				logErr := errx.Logic{}
 				ms.Suite.Require().ErrorAs(rErr, &logErr)
-				ms.Suite.Require().Len(logErr.Code(), model.ErrEventDateBusyCode)
+				ms.Suite.Require().Equal(model.ErrEventDateBusyCode, logErr.Code())
 			},
 		}, {
 			name: "event wrong data",
@@ -262,17 +265,17 @@ func (ms *MainSuiteTest) TestUpdate() {
 				NotifyTerm: &nfTermErr,
 			},
 			ID: events[0].ID,
-			checkResponseBody: func(rErr error) {
+			checkResponse: func(rErr error) {
 				invErr := errx.Invalid{}
 				ms.Suite.Require().ErrorAs(rErr, &invErr)
-				ms.Suite.Require().Len(invErr.Errors, 3)
+				ms.Suite.Require().Len(invErr.Errors(), 3)
 			},
 		},
 	}
 
 	for _, tc := range testCases {
 		ms.Suite.Run(tc.name, func() {
-			tc.checkResponseBody(ms.client.Update(ctx, tc.ID, tc.inputUpdate))
+			tc.checkResponse(ms.client.Update(ctx, tc.ID, tc.inputUpdate))
 		})
 	}
 }
@@ -284,153 +287,214 @@ func (ms *MainSuiteTest) TestDelete() {
 	dateInFuture := time.Now().Add(time.Hour * 24 * 30)
 	newEvent, err := ms.client.Create(ctx, dto.EventCreate{
 		Title:    "Test event",
-		Date:     dateInFuture.String(),
+		Date:     dateInFuture.Format(time.RFC3339),
 		Duration: "45m",
 	})
 	ms.Suite.Require().NotNil(newEvent)
 	ms.Suite.Require().NoError(err)
 
 	ms.Suite.Run("removing exists event", func() {
-		var event *dto.Event
-
 		err = ms.client.Delete(ctx, newEvent.ID)
 		ms.Suite.Require().NoError(err)
 
-		event, err = ms.client.GetByID(ctx, newEvent.ID)
-		ms.Suite.Require().Nil(event)
+		_, err = ms.client.GetByID(ctx, newEvent.ID)
 		rErr := errx.NotFound{}
 		ms.Suite.Require().ErrorAs(err, &rErr)
 	})
 
 	ms.Suite.Run("removing not exists event", func() {
 		err = ms.client.Delete(ctx, newEvent.ID)
-		ms.Suite.Require().NoError(err)
 		rErr := errx.NotFound{}
 		ms.Suite.Require().ErrorAs(err, &rErr)
 	})
 }
 
-/*
 func (ms *MainSuiteTest) TestList() {
-	events := addEvents(es, [][]byte{
-		[]byte(`{
-				"title": "Test event 1",
-				"date": "2023-02-19T20:00:00.417Z",
-				"duration": "45m"
-			}`),
-		[]byte(`{
-				"title": "Test event 2",
-				"date": "2023-02-19T09:00:00.417Z",
-				"duration": "90m"
-			}`),
-		[]byte(`{
-				"title": "Test event 3",
-				"date": "2023-02-13T15:00:00.417Z",
-				"duration": "60m"
-			}`),
-		[]byte(`{
-				"title": "Test event",
-				"date": "2023-03-10T10:00:00.417Z",
-				"duration": "45m"
-			}`),
-		[]byte(`{
-				"title": "Test event",
-				"date": "2023-03-22T20:00:00.417Z",
-				"duration": "45m",
-				"description": "№348239",
-				"notifyTerm": "3h"
-			}`),
-	})
-
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
+	defer cancel()
+	insertInput := []dto.EventCreate{
+		{
+			Title:    "Test event 1",
+			Date:     "2023-02-19T20:00:00.417Z",
+			Duration: "45m",
+		}, {
+			Title:    "Test event 2",
+			Date:     "2023-02-19T09:00:00.417Z",
+			Duration: "90m",
+		}, {
+			Title:    "Test event 3",
+			Date:     "2023-02-13T15:00:00.417Z",
+			Duration: "60m",
+		}, {
+			Title:    "Test event 4",
+			Date:     "2023-03-10T10:00:00.417Z",
+			Duration: "45m",
+		}, {
+			Title:    "Test event 5",
+			Date:     "2023-03-22T20:00:00.417Z",
+			Duration: "45m",
+		},
+	}
+	events := make([]*dto.Event, len(insertInput))
+	for i, input := range insertInput {
+		event, err := ms.client.Create(ctx, input)
+		ms.Suite.Require().NotNil(event)
+		ms.Suite.Require().NoError(err)
+		ms.eventIds = append(ms.eventIds, event.ID)
+		events[i] = event
+	}
 	testCases := []struct {
-		name         string
-		rangeType    string
-		date         string
-		expectedCode int
-		expectedIDs  []string
+		name          string
+		rangeType     string
+		date          string
+		checkResponse func(error)
+		expectedIDs   []string
 	}{
 		{
-			name:         "day (whole 2023-02-19) events",
-			rangeType:    "day",
-			date:         "2023-02-19T22:00:00.417Z",
-			expectedCode: http.StatusOK,
+			name:      "day (whole 2023-02-19) events",
+			rangeType: "day",
+			date:      "2023-02-19T22:00:00.417Z",
 			expectedIDs: []string{
 				events[0].ID,
 				events[1].ID,
 			},
+			checkResponse: func(rErr error) {
+				ms.Suite.Require().NoError(rErr)
+			},
 		}, {
-			name:         "month (whole march) events",
-			rangeType:    "month",
-			date:         "2023-03-19T22:00:00.417Z",
-			expectedCode: http.StatusOK,
+			name:      "month (whole march) events",
+			rangeType: "month",
+			date:      "2023-03-19T22:00:00.417Z",
 			expectedIDs: []string{
 				events[3].ID,
 				events[4].ID,
 			},
+			checkResponse: func(rErr error) {
+				ms.Suite.Require().NoError(rErr)
+			},
 		}, {
-			name:         "week feb 13-19",
-			rangeType:    "week",
-			date:         "2023-02-15T22:00:00.417Z",
-			expectedCode: http.StatusOK,
+			name:      "week feb 13-19",
+			rangeType: "week",
+			date:      "2023-02-15T22:00:00.417Z",
 			expectedIDs: []string{
 				events[0].ID,
 				events[1].ID,
 				events[2].ID,
 			},
+			checkResponse: func(rErr error) {
+				ms.Suite.Require().NoError(rErr)
+			},
 		}, {
-			name:         "empty list",
-			rangeType:    "week",
-			date:         "2023-02-25T22:00:00.417Z",
-			expectedCode: http.StatusOK,
-			expectedIDs:  []string{},
+			name:        "empty list",
+			rangeType:   "week",
+			date:        "2023-02-25T22:00:00.417Z",
+			expectedIDs: []string{},
+			checkResponse: func(rErr error) {
+				ms.Suite.Require().NoError(rErr)
+			},
 		}, {
-			name:         "wrong range type",
-			rangeType:    "hour",
-			date:         "2023-02-25T22:00:00.417Z",
-			expectedCode: http.StatusBadRequest,
-			expectedIDs:  []string{},
+			name:        "wrong range type",
+			rangeType:   "hour",
+			date:        "2023-02-25T22:00:00.417Z",
+			expectedIDs: []string{},
+			checkResponse: func(rErr error) {
+				logErr := errx.Logic{}
+				ms.Suite.Require().ErrorAs(rErr, &logErr)
+				ms.Suite.Require().Equal(model.ErrCalendarDateRangeCode, logErr.Code())
+			},
 		}, {
-			name:         "wrong date",
-			rangeType:    "day",
-			date:         "-------2023-02-25T22:00:00.417Z-----",
-			expectedCode: http.StatusBadRequest,
-			expectedIDs:  []string{},
+			name:        "wrong date",
+			rangeType:   "day",
+			date:        "-------2023-02-25T22:00:00.417Z-----",
+			expectedIDs: []string{},
+			checkResponse: func(rErr error) {
+				logErr := errx.Logic{}
+				ms.Suite.Require().ErrorAs(rErr, &logErr)
+				ms.Suite.Require().Equal(model.ErrCalendarDateRangeCode, logErr.Code())
+			},
 		},
 	}
 	for _, tc := range testCases {
-		es.Suite.Run(tc.name, func() {
-			ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
-			defer cancel()
-			requestURL := fmt.Sprintf(
-				"%s/events/list/%s?date=%s",
-				es.testServer.URL,
-				tc.rangeType,
-				url.QueryEscape(tc.date),
-			)
-			req, err := http.NewRequestWithContext(ctx, http.MethodGet, requestURL, nil)
-			es.Suite.Require().NoError(err)
-			req.Header.Set("Authorization", ValidUserEmail)
-
-			res, err := http.DefaultClient.Do(req)
-			es.Suite.Require().NoError(err)
-			defer func() {
-				_ = res.Body.Close()
-			}()
-			es.Suite.Require().Equal(tc.expectedCode, res.StatusCode)
-			if tc.expectedCode == http.StatusOK {
-				var resp []dto.Event
-				actualIDs := make([]string, 0)
-				err = json.NewDecoder(res.Body).Decode(&resp)
-				es.Suite.Require().NoError(err)
-				for _, event := range resp {
-					actualIDs = append(actualIDs, event.ID)
-				}
-				es.Suite.Require().Equal(tc.expectedIDs, actualIDs)
+		ms.Suite.Run(tc.name, func() {
+			date, _ := time.Parse(time.RFC3339, tc.date)
+			events, err := ms.client.GetListOnDate(ctx, tc.rangeType, date)
+			actualIDs := make([]string, len(events))
+			for i, event := range events {
+				actualIDs[i] = event.ID
 			}
+			ms.Suite.Require().Equal(tc.expectedIDs, actualIDs)
+
+			tc.checkResponse(err)
 		})
 	}
 }
-*/
+
+func (ms *MainSuiteTest) TestNotify() {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*15)
+	defer cancel()
+
+	dateNowDays := time.Now().Add(time.Minute * 20)
+	dateIndaFuture := time.Now().Add(time.Hour * 2)
+	notifyTerm := "30m"
+
+	insertInput := []dto.EventCreate{
+		{
+			Title:      "Event must to be notified",
+			Date:       dateNowDays.Format(time.RFC3339),
+			Duration:   "45m",
+			NotifyTerm: &notifyTerm,
+		}, {
+			Title:      "Event must not to be notified",
+			Date:       dateIndaFuture.Format(time.RFC3339),
+			Duration:   "45m",
+			NotifyTerm: &notifyTerm,
+		},
+	}
+	events := make([]*dto.Event, len(insertInput))
+	for i, input := range insertInput {
+		event, err := ms.client.Create(ctx, input)
+		ms.Suite.Require().NotNil(event)
+		ms.Suite.Require().NoError(err)
+		ms.eventIds = append(ms.eventIds, event.ID)
+		events[i] = event
+	}
+
+	testCases := []struct {
+		name           string
+		ID             string
+		notifiedStatus model.NotifyStatus
+	}{
+		{
+			name:           "event must to be notified",
+			ID:             events[0].ID,
+			notifiedStatus: model.NotifyStatusNotified,
+		}, {
+			name:           "Event must not to be notified",
+			ID:             events[1].ID,
+			notifiedStatus: model.NotifyStatusNone,
+		},
+	}
+
+	// waiting for notifier trigger
+	select {
+	case <-ctx.Done():
+		ms.Suite.Require().True(false, "testing adding or timer is too long")
+	case <-time.After(time.Second * 6):
+	}
+
+	for _, tc := range testCases {
+		ms.Suite.Run(tc.name, func() {
+			event, err := ms.client.GetByID(ctx, tc.ID)
+			ms.Suite.Require().NoError(err)
+			ms.Suite.Require().NotNil(event)
+
+			notifyStatus, err := model.ParseNotifyStatus(event.NotifyStatus)
+			ms.Suite.Require().NoError(err)
+
+			ms.Suite.Require().Equal(notifyStatus, tc.notifiedStatus)
+		})
+	}
+}
 
 func TestCalendar(t *testing.T) {
 	suite.Run(t, new(MainSuiteTest))
