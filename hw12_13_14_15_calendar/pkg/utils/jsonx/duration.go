9f7ecf4bf64c9dc40a909c2e9193ab6db6741480
Vitaly Yermakov <vit_ermakov@mail.ru>
2023-01-21 02:50:22 +0300

    custom duration

---
+++ hw12_13_14_15_calendar/pkg/utils/jsonx/duration.go
@@ -0,0 +1,104 @@
+package jsonx
+
+import (
+	"errors"
+	"fmt"
+	"strconv"
+	"time"
+)
+
+const QuotesByte = 34
+
+type Duration struct {
+	value int
+	unit  byte
+}
+
+func NewDuration(value int, unit byte) Duration {
+	return Duration{value, unit}
+}
+
+var allowedDs = map[byte]time.Duration{
+	's': time.Second,
+	'm': time.Minute,
+	'h': time.Hour,
+	'd': time.Hour * 24,
+	'w': time.Hour * 24 * 7,
+	'n': time.Hour * 24 * 30,
+	'y': time.Hour * 24 * 365,
+}
+
+// UnmarshalJSON разрешенный формат <duration int >= 0><unit rune>. Allowed units:
+// - s second
+// - m minute
+// - h hour
+// - d day
+// - w week
+// - n 30 days
+// - y 365 days
+func (d *Duration) UnmarshalJSON(data []byte) error {
+	var err error
+	if data[0] == QuotesByte {
+		*d, err = ParseDuration(string(data[1 : len(data)-1]))
+	} else {
+		*d, err = ParseDuration(string(data))
+	}
+	return err
+}
+
+func (d Duration) MarshalJSON() ([]byte, error) {
+	return []byte(d.String()), nil
+}
+
+func (d Duration) Valid() bool {
+	_, ok := allowedDs[d.unit]
+	return ok
+}
+
+func (d Duration) String() string {
+	return fmt.Sprintf("%d%s", d.value, string(d.unit))
+}
+
+func (d Duration) AsDuration() (time.Duration, error) {
+	if !d.Valid() {
+		return time.Second * 0, errors.New("not valid duration")
+	}
+	return time.Duration(d.value) * allowedDs[d.unit], nil
+}
+
+func ParseDuration(data string) (Duration, error) {
+	var (
+		s = make([]byte, 0, len(data))
+		u = make([]byte, 0, len(data))
+	)
+	var unit bool
+	for _, ch := range data {
+		if unit {
+			u = append(u, byte(ch))
+		} else {
+			if ch >= '0' && ch <= '9' {
+				s = append(s, byte(ch))
+			} else {
+				u = append(u, byte(ch))
+				unit = true
+			}
+		}
+	}
+	if len(s) == 0 {
+		return Duration{}, fmt.Errorf("no value: %s", data)
+	}
+	n, err := strconv.Atoi(string(s))
+	if err != nil || n < 0 {
+		return Duration{}, fmt.Errorf("wrong value: %s", s)
+	}
+	if len(u) == 0 {
+		u = append(u, 's')
+	}
+	if len(u) != 1 {
+		return Duration{}, fmt.Errorf("wrong unit: %s", u)
+	}
+	if _, ok := allowedDs[u[0]]; !ok {
+		return Duration{}, fmt.Errorf("unknown unit: %s", u)
+	}
+	return Duration{n, u[0]}, nil
+}
---
+++ hw12_13_14_15_calendar/pkg/utils/jsonx/duration_test.go
@@ -0,0 +1,91 @@
+package jsonx
+
+import (
+	"fmt"
+	"github.com/stretchr/testify/require"
+	"testing"
+	"time"
+)
+
+func TestParseDuration(t *testing.T) {
+	testCases := []struct {
+		input    string
+		expected Duration
+		isErr    bool
+		duration time.Duration
+	}{
+		{
+			input:    "3er",
+			expected: Duration{},
+			isErr:    true,
+		}, {
+			input:    "b3",
+			expected: Duration{},
+			isErr:    true,
+		}, {
+			input:    "3y6",
+			expected: Duration{},
+			isErr:    true,
+		}, {
+			input:    "-1y",
+			expected: Duration{},
+			isErr:    true,
+		}, {
+			input:    "m",
+			expected: Duration{},
+			isErr:    true,
+		}, {
+			input:    "5s",
+			expected: Duration{},
+			isErr:    false,
+			duration: time.Second * 5,
+		}, {
+			input:    "50",
+			expected: Duration{},
+			isErr:    false,
+			duration: time.Second * 50,
+		}, {
+			input:    "5m",
+			expected: Duration{},
+			isErr:    false,
+			duration: time.Minute * 5,
+		}, {
+			input:    "5h",
+			expected: Duration{},
+			isErr:    false,
+			duration: time.Hour * 5,
+		}, {
+			input:    "5d",
+			expected: NewDuration(5, 'd'),
+			isErr:    false,
+			duration: time.Hour * 24 * 5,
+		}, {
+			input:    "5w",
+			expected: NewDuration(5, 'w'),
+			isErr:    false,
+			duration: time.Hour * 24 * 7 * 5,
+		}, {
+			input:    "5n",
+			expected: NewDuration(5, 'n'),
+			isErr:    false,
+			duration: time.Hour * 24 * 30 * 5,
+		}, {
+			input:    "5y",
+			expected: NewDuration(5, 'y'),
+			isErr:    false,
+			duration: time.Hour * 24 * 365 * 5,
+		},
+	}
+	for i, tc := range testCases {
+		t.Run(fmt.Sprintf("test %d", i+1), func(t *testing.T) {
+			d, err := ParseDuration(tc.input)
+			if tc.isErr {
+				require.Error(t, err)
+			} else {
+				require.NoError(t, err)
+				ds, _ := d.AsDuration()
+				require.Equal(t, tc.duration, ds, "(%d, %s)", d.value, string(d.unit))
+			}
+		})
+	}
+}
